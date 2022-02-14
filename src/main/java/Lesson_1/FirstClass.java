b55450ef6536cca8ca6b0155198377758f0ac932
Andew <tarnavsky.andrew@gmail.com>
2022-02-14 21:10:54 +0200

    add file SecondClass.java_homeWork

---
+++ src/main/java/Lesson_1/FirstClass.java
@@ -0,0 +1,23 @@
+//package Lesson_1;
+//
+//public class FirstClass {
+//
+//    //FirstClass FirstClass = new FirstClass();
+//    public static String returnGreeting(int year) {
+//        String pattern = "---%s---";
+//        return String.format(pattern, year);
+//        FirstClass FirstClass = new FirstClass();
+//        System.out.println(FirstClass.returnGreeting(2025));
+//
+//
+//    }
+//
+//
+//
+//    public static void main(String[] args) {
+//
+
+//    }
+//
+//
+//}
