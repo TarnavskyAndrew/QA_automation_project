6b2df13eea5bf4fd804dcc1112eff1f8423c0473
Andew <tarnavsky.andrew@gmail.com>
2022-02-17 15:09:58 +0200

    changes in file_OperatorClass

--- src/main/java/Lesson_1/OperatorClass.java
+++ src/main/java/Lesson_1/OperatorClass.java
@@ -8,12 +8,14 @@ public class OperatorClass {
 
         int m = (x+x)+(x+x);
         boolean b = (x+x)<(++x+x);
-        int m3 = ((x--)+x);
+        //int m3 = ((x--)+x); правильно: стр. 12, 13
+        int m3 = 2*x - 1;
+        x--;
         int m4 = (--x+x);
         int m5 = m3+m4;
         boolean b1 = m3>m4;
-        boolean b2 = (m3%2)>0;
-        boolean b3 = (m4%2)>0;
+        boolean b2 = (m3%2)==0;
+        boolean b3 = (m4%2)==0;
 
 
         System.out.println(m);
@@ -23,5 +25,12 @@ public class OperatorClass {
         System.out.println(m5);
         System.out.println(b1);
         System.out.println(b2 || b3);
+
+//        System.out.println( );
+//        boolean c = (10%2)==0;
+//        boolean c1 = (13%2)==0;
+//        System.out.println(c);
+//        System.out.println(c1);
+//        System.out.println(c || c1);
     }
 }
